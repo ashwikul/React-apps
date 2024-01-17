6d0d51a10930d90ef3a8e820a45177748365c3a2
ashwikul <ashwinikk1990@gmail.com>
2024-01-17 13:44:49 +0530

    add reset button

--- stopwatch/src/App.js
+++ stopwatch/src/App.js
@@ -3,12 +3,15 @@ import Button from "./components/Button";
 import { useState } from 'react';
 
 function App() {
-  const [counter, setCounter] = useState(0);
+  const [counterOne, setcounterOne] = useState(0);
+  const [counterTwo, setcounterTwo] = useState(0);
+  const [intervalIdTwo, setIntervalIdTwo] = useState(0);
+  const [isTimerOn, setIsTimerOn] = useState(false);
   const [intervalId, setIntervalId] = useState(0);
   const increment = () => {
     clearInterval(intervalId) //clearing the previous interval before adding new interval
     const newIntervalId = setInterval(() => {
-      setCounter((prev) => prev + 1)
+      setcounterOne((prev) => prev + 1)
     }, 1000);
     setIntervalId(newIntervalId)
   }
@@ -16,12 +19,36 @@ function App() {
     clearInterval(intervalId);
   }
 
+  const startTimer = () => {
+    setIsTimerOn(true);
+    clearInterval(intervalIdTwo);
+    const intervalId = setInterval(() => {
+      setcounterTwo(prev => prev + 1);
+    }, 1000);
+    setIntervalIdTwo(intervalId)
+  }
+
+  const stopTimer = () => {
+    clearInterval(intervalIdTwo)
+    setIsTimerOn(false)
+  }
+
+  const resetTimer = () => {
+    clearInterval(intervalIdTwo)
+    setIsTimerOn(false)
+    setcounterTwo(0)
+  }
+
   return (
     <div>
       <h1>Stopwatch Timer</h1>
-      <h1>{counter}</h1>
+      <h1>{counterOne}</h1>
       <Button btnText="start" btnHandler={increment} />
       <Button btnText="stop" btnHandler={decrement} />
+      <h1>Stopwatch Timer with reset</h1>
+      <h1>{counterTwo}</h1>
+      {isTimerOn ? <Button btnText="stop" btnHandler={stopTimer} /> : <Button btnText="start" btnHandler={startTimer} />}
+      <Button btnText="reset" btnHandler={resetTimer} />
     </div>
   );
 }
